8ae5841517f3680d4ba3b74de00573962963d822
zrq <1258303476@qq.com>
2017-07-28 17:24:25 +0800

    两数之和题目代码提交

---
+++ 两数之和.java
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    /*
+     * @param numbers : An array of Integer
+     * @param target : target = numbers[index1] + numbers[index2]
+     * @return : [index1 + 1, index2 + 1] (index1 < index2)
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        // write your code here
+        int []index = new int[2];
+        index[0] = index[1] = 0;
+        for (int i = 0; i < numbers.length - 1; i++){
+            for (int j = i + 1; j < numbers.length; j++){
+                if (target == numbers[i] + numbers[j]){
+                    index[0] = i + 1;
+                    index[1] = j + 1;
+                    return index;
+                }
+            }
+        }
+        return index;
+    }
+}
