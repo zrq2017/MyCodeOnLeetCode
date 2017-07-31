f6fc1da8e0d66355f1f2b62c622d55fb3cb85c42
zrq <1258303476@qq.com>
2017-07-31 12:53:22 +0800

    链表求和及目录结构调整

---
+++ Java/链表求和.java
@@ -0,0 +1,43 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;      
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param l1: the first list
+     * @param l2: the second list
+     * @return: the sum list of l1 and l2 
+     */
+    public ListNode addLists(ListNode l1, ListNode l2) {
+        // write your code here
+        ListNode rst = new ListNode(0);
+        ListNode dummy = rst;
+        int carrier = 0;
+        //while
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                carrier += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                carrier += l2.val;
+                l2 = l2.next;
+            }
+            rst.next = new ListNode(carrier % 10);
+            carrier = carrier / 10;
+            rst = rst.next;
+        }
+        //check the carrier
+        if (carrier == 1) {
+            rst.next = new ListNode(1);
+        }
+        return dummy.next;
+    }
+}
