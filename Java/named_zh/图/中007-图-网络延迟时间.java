f585f4d8c7ea358b02123166e3b97303ab083fb5
zrq2017 <1258303476@qq.com>
2019-05-09 20:16:24 +0800

    网络延迟时间:动手练习弗洛伊德算法求最短距离

--- Java/named_zh/图/中007-图-网络延迟时间.java
+++ Java/named_zh/图/中007-图-网络延迟时间.java
@@ -63,33 +63,82 @@ class Solution {
         }
         return maxRes == Integer.MAX_VALUE? -1 : maxRes;
     }
+	//练习的DisjKstra算法
+	public int networkDelayTimeDisjKstra(int[][] times,int N,int K){
+        int len=N+1;
+        int[][] grid=new int [len][len];
+        int[] dist=new int[len];
+        for(int i=1;i<len;i++){
+            dist[i]=Integer.MAX_VALUE;//初始化每个点的最大距离
+        }
+        for(int i=1;i<len;i++){//点从1-N
+            for(int j=1;j<len;j++){
+                grid[i][j]=-1;//初始化图的邻接矩阵空间
+            }
+        }
+        for(int i=0;i<times.length;i++){
+            grid[times[i][0]][times[i][1]]=times[i][2];//根据边为图赋值
+        }
+        dist[K]=0;//初始时的点距离为0
+        ArrayDeque<Integer> deque=new ArrayDeque<>();//设置出点的队列
+        deque.add(K);//起始点入队
+        while(!deque.isEmpty()){//当队列不空时更新该点能到达的边的距离
+            int source=deque.getFirst();
+            for(int i=1;i<len;i++){
+                if(grid[source][i]!=-1 && grid[source][i]+dist[source]<dist[i]){//不等于-1代表两点有边，且由源点到终点的距离更小
+                    dist[i]=grid[source][i]+dist[source];
+                    deque.add(i);
+                }
+            }
+            deque.pop();
+        }
+        int max=0;//记录要花的时间，最大值为Integer.MAX_VALUE代表有点没访问过
+        for(int i=1;i<len;i++){
+            max=Math.max(max,dist[i]);
+        }
+        return max==Integer.MAX_VALUE?-1:max;
+    }
 }
 //Floyd-Warshall 算法
 class Solution {
+    /**
+     * 弗洛伊德算法Floyd-Warshall
+     * 每个i-j的点经过k，然后从1-N比较是否Di,j>Di,k+Dk,j 即经过k点使得i-j距离更小那就更新最小距离
+     *  for k from 1 to |V|
+     *     for i from 1 to |V|
+     *        for j from 1 to |V|
+     *           if dist[i][j] > dist[i][k] + dist[k][j]
+     *              dist[i][j] ← dist[i][k] + dist[k][j]
+     *          end if
+     * @param times
+     * @param N
+     * @param K
+     * @return
+     */
     public int networkDelayTime(int[][] times, int N, int K) {
         /*
         本质上是求单点到所有其他点的最短距离，最后求最短距离中的最大值。
         使用Floyd-Warshall 算法求解。
         */
-        K--;  //输入中节点编号是基1，故减1
-        int[] dis = new int[N];  //存储K到每个点的最短距离
-        for(int i=0; i<N; i++){
-            dis[i] = Integer.MAX_VALUE;
+		int len=N+1;
+        int[] dist=new int[len];
+        for(int i=1;i<len;i++){
+            dist[i]=Integer.MAX_VALUE;//每个点到达的最短距离
         }
-        dis[K] = 0;
-        for(int i=0; i<N; i++){
-            for(int[] edge: times){
-                //输入中节点编号是基1，故减1
-                int u = edge[0]-1, v = edge[1]-1, w = edge[2];
-                if(dis[u] != Integer.MAX_VALUE && dis[v] > dis[u]+w)
-                    dis[v] = dis[u] + w;
+        dist[K]=0;//开始节点最短为0
+        for(int i=1;i<len;i++){//点1-N
+            for(int[] edge:times){
+                int u=edge[0],v=edge[1],w=edge[2];
+                if(dist[u]!=Integer.MAX_VALUE && dist[v]>dist[u]+w){//第一个不等于最大值的条件保证了由入点K开始更新最短距离，后面的思想就是相当于不断消耗edge边，就算下次碰上同样的边也不变，只有更小的边才会减小
+                    //由源点u到终点v增加的距离比原来到终点v的距离更短
+                    dist[v]=dist[u]+w;
+                }
             }
         }
-        //求最短距离中的最大值
-        int res = 0;
-        for(int i=0; i<N; i++){
-            res = Math.max(res, dis[i]);
+        int max=0;//判断最短距离，有没访问过的点那就是Integer.MAX_VALUE
+        for(int i=1;i<len;i++){
+            max=Math.max(max,dist[i]);
         }
-        return res == Integer.MAX_VALUE? -1: res;        
+        return max==Integer.MAX_VALUE?-1:max;
     }
 }
