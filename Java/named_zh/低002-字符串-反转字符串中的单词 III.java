babea3f8b79d3115a832c7b5f22ecbf0f7cfb00b
zrq2017 <1258303476@qq.com>
2019-05-25 16:10:29 +0800

    反转字符串中的单词 III(双指针解法)

--- Java/named_zh/低002-字符串-反转字符串中的单词 III.java
+++ Java/named_zh/低002-字符串-反转字符串中的单词 III.java
@@ -18,7 +18,30 @@ public class ReverseWords {
         String s="Let's take LeetCode contest";
         reverseWords(s);
     }
+
+    //双指针解法：一个指向当前单词头，一个顺序往后走，碰到空格做单词的翻转
     public static String reverseWords(String s) {
+        int len=s.length(),p=0,q=0;//p为移动指针，q为上一个空格后的位置指针
+        StringBuilder sb=new StringBuilder();
+        while(p<len){
+            if(s.charAt(p)==' '){
+                for(int i=p-1;i>=q;i--){
+                    sb.append(s.charAt(i));
+                }
+                q=p+1;//将q移到下一个单词开始位置
+                sb.append(s.charAt(p++));
+            }
+            p++;
+        }
+        if(q<len){//将最后一个单词加入，因为最后一个单词不是以空格分割为结尾的
+            for(int i=len-1;i>=q;i--){
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+    //普通解法：翻转字符串后按空格分割然后翻转单个单词
+    public static String reverseWordsEasy(String s) {
         StringBuilder sb=new StringBuilder();
         for(int i=s.length()-1;i>=0;i--){//翻转整个字符串
             sb.append(s.charAt(i));
